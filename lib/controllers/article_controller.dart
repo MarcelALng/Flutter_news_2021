db9df69bc0563983161d280856e7dfc830de72fa
Marcel AL NG <43136183+MarcelALng@users.noreply.github.com>
2021-05-04 16:28:27 +0200

    [SDK] back to 1.22 works ok

--- lib/controllers/article_controller.dart
+++ lib/controllers/article_controller.dart
@@ -1,25 +1,27 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_news2021/models/article_model.dart';
 
 class ArticleController extends ChangeNotifier {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final Firestore _firestore = Firestore.instance;
+
+  ArticleController() {
+    getArticles();
+  }
 
   Future getArticles() async {
     QuerySnapshot _dataFirestore = await _firestore
         .collection("articles")
         .orderBy("date", descending: true)
-        .get(); //getDocuments in older version
+        .getDocuments(); //getDocuments in older version
 
-    List<ArticleModel> _articlesData = _dataFirestore.docs
+    List<ArticleModel> _articlesData = _dataFirestore.documents
         .map((article) => ArticleModel(
-            title: article.data()["name"],
-            description: article.data()["description"],
-            imageUrl: article.data()["preview"],
-            date: article.data()["date"]))
+            title: article.data["name"],
+            description: article.data["description"],
+            imageUrl: article.data["preview"],
+            date: article.data["date"]))
         .toList();
   }
 }
